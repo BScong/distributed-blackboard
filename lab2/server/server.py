e54ef75a0a69a6989de1c0bd39108243d5951b9e
bscong <benoit.zhong@gmail.com>
2017-11-13 23:05:02 +0100

    Refactoring + centralized ADD/MOD/DEL

--- lab2/server/server.py
+++ lab2/server/server.py
@@ -47,14 +47,16 @@ class BlackboardServer(HTTPServer):
 		self.vessel_id = vessel_id
 		# The list of other vessels
 		self.vessels = vessel_list
+		# The leader id
+		self.leader_id = 3
 #------------------------------------------------------------------------------------------------------
-	# We add a value received to the store
+	# We add a value received to the store - This function is only used by the leader
 	def add_value_to_store(self, value):
 		self.current_key += 1
 		self.store[self.current_key] = value
-
+		return self.current_key
 #------------------------------------------------------------------------------------------------------
-	# We modify a value received in the store
+	# We modify a value received in the store - This function is only used by the leader
 	def modify_value_in_store(self,key,value):
 		# we modify a value in the store if it exists
 		if key in self.store:
@@ -65,7 +67,18 @@ class BlackboardServer(HTTPServer):
 		# we delete a value in the store if it exists
 		if key in self.store:
 			del self.store[key]
-		pass
+		
+#------------------------------------------------------------------------------------------------------
+	# We set a value that the leader gave us - This function is only used by the slaves
+	def set_value_in_local_store(self, key, value):
+		if key not in self.store:
+			self.store[key]=""
+		self.store[key]=value
+
+		# we update the current key
+		if key > self.current_key:
+			self.current_key = key
+
 #------------------------------------------------------------------------------------------------------
 # Contact a specific vessel with a set of variables to transmit to it
 	def contact_vessel(self, vessel_ip, path, action, key, value):
@@ -102,13 +115,36 @@ class BlackboardServer(HTTPServer):
 #------------------------------------------------------------------------------------------------------
 	# We send a received value to all the other vessels of the system
 	def propagate_value_to_vessels(self, path, action, key, value):
+		retry_list = [] # we will store the vessels without success
 		# We iterate through the vessel list
 		for vessel in self.vessels:
 			# We should not send it to our own IP, or we would create an infinite loop of updates
 			if vessel != ("10.1.0.%s" % self.vessel_id):
-				# A good practice would be to try again if the request failed
-				# Here, we do it only once
-				self.contact_vessel(vessel, path, action, key, value)		
+				success = self.contact_vessel(vessel, path, action, key, value)
+				if not success:
+					retry_list.append(vessel)
+
+		# for the vessels who failed, we retry
+		try_count = 0
+		while len(retry_list)>0 and try_count < 15:
+			try_count += 1
+			for vessel in retry_list:
+				success = self.contact_vessel(vessel, path, action, key, value)
+				if success : retry_list.remove(vessel)
+		if len(retry_list)>0:
+			print("Problem occured with " + ", ".join(retry_list) + " : Unable to connect after multiple attempts.")
+
+	# Function to forward the request to the leader
+	def forward_request_to_leader(self, path, action, key, value):
+		leader_ip = "10.1.0."+str(self.leader_id)
+		success = False
+		try_count = 0
+		while success == False and try_count < 10: 
+			try_count += 1 # If the request fails, we retry (10 retry max)
+			success = self.contact_vessel(leader_ip,path,action,key,value)
+		if success == False:
+			print("Problem occured with " + leader_ip + " : Unable to join after multiple attempts.")
+		
 #------------------------------------------------------------------------------------------------------
 
 
@@ -162,9 +198,8 @@ class BlackboardRequestHandler(BaseHTTPRequestHandler):
 	def do_GET_Index(self):
 		# We set the response status code to 200 (OK)
 		self.set_HTTP_headers(200)
-		# We should do some real HTML here
-		# html_response = "<html><head><title>Basic Skeleton</title></head><body>This is the basic HTML content when receiving a GET</body></html>"
-		#In practice, go over the entries list, 
+	
+		#We go over the entries list, 
 		#produce the boardcontents part, 
 		#then construct the full page by combining all the parts ...
 
@@ -188,81 +223,142 @@ class BlackboardRequestHandler(BaseHTTPRequestHandler):
 
 
 #------------------------------------------------------------------------------------------------------
-	# we might want some other functions
-#------------------------------------------------------------------------------------------------------
-#------------------------------------------------------------------------------------------------------
 # Request handling - POST
 #------------------------------------------------------------------------------------------------------
 	def do_POST(self):
 		print("Receiving a POST on %s" % self.path)
-		# Here, we should check which path was requested and call the right logic based on it
-		# We should also parse the data received
-		# and set the headers for the client
-
-		retransmit = False # Like this, we won't create infinite loops!
-		# Var initialization for propagation
-		action = ""
-		key = -1
-		value = ""
 
 		parsed = self.parse_POST_request() # Parsing data
 		print(parsed)
 
-		# Self cases
-		if self.path == "/entries" and "entry" in parsed: # Adding entry from this server
-			retransmit = True # Propagate
-			action = "ADD"
-			value = parsed['entry'][0]
-			self.server.add_value_to_store(value) # Adding entry to server
-			self.set_HTTP_headers(200)
-		elif "/entries" in self.path and "entry" in parsed and "delete" in parsed and parsed['delete'][0]=='0':
-			retransmit = True
-			action = "MOD"
-			value = parsed['entry'][0]
-			key = int(self.path.replace("/entries/",""))
-			self.server.modify_value_in_store(key,value)
-			self.set_HTTP_headers(200)
-		elif "/entries" in self.path and "delete" in parsed and parsed['delete'][0]=='1':
-			retransmit = True
-			action = "DEL"
-			key = int(self.path.replace("/entries/",""))
-			self.server.delete_value_in_store(key)
-			self.set_HTTP_headers(200)
-
-		# Propagate cases
-		elif self.path == "/entries" and "action" in parsed and "value" in parsed and parsed['action'][0] == "ADD" : # Entry added by another server
-			print("Adding entry")
-			self.server.add_value_to_store(parsed['value'][0]) 
-			self.set_HTTP_headers(200)
-		elif self.path == "/entries" and "action" in parsed and "key" in parsed and "value" in parsed and parsed['action'][0] == "MOD" :
-			print("Modifying entry")
-			key = int(parsed['key'][0])
-			self.server.modify_value_in_store(key,parsed['value'][0]) 
-			self.set_HTTP_headers(200)
-		elif self.path == "/entries" and "action" in parsed and "key" in parsed and parsed['action'][0] == "DEL" :
-			print("Deleting entry")
-			key = int(parsed['key'][0])
-			self.server.delete_value_in_store(key) 
-			self.set_HTTP_headers(200)
+		# Check if the path is correct
+		valid_path_entries = True if "/entries" in self.path else False
+		
+		if valid_path_entries:
+			self.do_POST_entries(parsed)
+		# TODO : Here we must implement the POST Logic for the Leader Election
 		else:
 			self.set_HTTP_headers(400)
-		
-		# If we want to retransmit what we received to the other vessels
-		
-		if retransmit:
+#------------------------------------------------------------------------------------------------------
+# POST Logic
+#------------------------------------------------------------------------------------------------------
+	
+	# POST Logic for entries (slave/leader)
+	def do_POST_entries(self, parsed):
+		# Fetch parameters
+		action, key, value, propagate = self.get_entries_parameters(parsed)
+		# action is in ["ADD","DEL","MOD","SET"].
+		# We added the action "SET" that sets a value for a specific key
+		# propagate is true if the request comes from me.
+		# (If the request is received from someone else, then propagate is false)
+
+		# Validate parameters
+		isValid = self.are_parameters_valid(action, key, value)
+
+		if isValid:
+			self.set_HTTP_headers(200)
+
+			# self.do_POST_entries_vessel(action,key,value,propagate) # This was used in propagate version (task 1)
+			
+			if self.server.vessel_id == self.server.leader_id : # If I am the leader
+				self.do_POST_entries_leader(action, key, value) # Directly do the action
+			elif not propagate : # If the request comes from the leader
+				self.do_POST_entries_slave(action, key, value)
+			else: # We forward the request we received to the leader
+				# do_POST send the message only when the function finishes
+				# We must then create threads if we want to do some heavy computation
+				thread = Thread(target=self.server.forward_request_to_leader,args=("/entries", action, key, value) )
+				# We kill the process if we kill the server
+				thread.daemon = True
+				# We start the thread
+				thread.start()
+
+		else:
+			self.set_HTTP_headers(400)
+
+	# Specific POST Logic for leader (centralized version)
+	def do_POST_entries_leader(self, action, key, value):
+		if action == "ADD" : # Entry added by another server
+			print("Adding entry")
+			action="SET"
+			key = self.server.add_value_to_store(value) 
+		elif action == "MOD" :
+			print("Modifying entry")
+			action="SET"
+			self.server.modify_value_in_store(key,value) 
+		elif action == "DEL" :
+			print("Deleting entry")
+			self.server.delete_value_in_store(key) 
+
+		thread = Thread(target=self.server.propagate_value_to_vessels,args=("/entries", action, key, value) )
+		# We kill the process if we kill the server
+		thread.daemon = True
+		# We start the thread
+		thread.start()
+
+	# Slave POST Logic in centralized version
+	def do_POST_entries_slave(self, action, key, value):
+		if action == "SET" :
+			forward = False # but don't forward if the order is from the leader
+			print("Entry from leader received")
+			self.server.set_value_in_local_store(key, value)
+		elif action =="DEL": 
+			forward = False # but don't forward if the order is from the leader
+			print("Delete order from leader received")
+			self.server.delete_value_in_store(key)
+
+
+	# Vessel POST Logic (Task 1)
+	def do_POST_entries_vessel(self, action, key, value, propagate):
+		if action == "ADD" : # Entry added by another server
+			print("Adding entry")
+			key = self.server.add_value_to_store(value) 
+		elif action == "MOD" :
+			print("Modifying entry")
+			self.server.modify_value_in_store(key,value) 
+		elif action == "DEL" :
+			print("Deleting entry")
+			self.server.delete_value_in_store(key)
+		# If we want to propagate the request to other vessels
+		if propagate:
 			# do_POST send the message only when the function finishes
 			# We must then create threads if we want to do some heavy computation
-			# 
-			# Random content
 			thread = Thread(target=self.server.propagate_value_to_vessels,args=("/entries", action, key, value) )
 			# We kill the process if we kill the server
 			thread.daemon = True
 			# We start the thread
-			thread.start()
-#------------------------------------------------------------------------------------------------------
-# POST Logic
-#------------------------------------------------------------------------------------------------------
-	# We might want some functions here as well
+			thread.start() 
+
+	def get_entries_parameters(self, params):
+		action, key, value, propagate = "",-1,"",True
+
+		if "action" in params: # Propagate cases
+			propagate = False
+			action = params['action'][0]
+			key = int(params['key'][0]) if "key" in params else ""
+			value = params['value'][0] if "value" in params else ""
+		else: # Self cases
+			key = int(self.path.replace("/entries/","")) if "/entries/" in self.path else -1
+			value = params['entry'][0] if "entry" in params else ""
+			if "delete" in params and params['delete'][0]=='1':
+				action = "DEL"
+			elif "delete" in params and params['delete'][0]=='0':
+				action = "MOD"
+			else:
+				action = "ADD"
+
+		return action, key, value, propagate
+
+	def are_parameters_valid(self, action, key, value):
+		if action=="ADD" and value!="":
+			return True
+		elif action == "DEL" and key!=-1:
+			return True
+		elif action in ["MOD","SET"] and key!=-1 and value!="":
+			return True
+		else:
+			return False
+
 #------------------------------------------------------------------------------------------------------
 
 
@@ -275,7 +371,6 @@ class BlackboardRequestHandler(BaseHTTPRequestHandler):
 if __name__ == '__main__':
 
 	## read the templates from the corresponding html files
-	# .....
 
 	vessel_list = []
 	vessel_id = 0
